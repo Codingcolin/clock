ff90373a019dd7e9c5363aeb013637595e421af1
bigc986 <c_jax31@yahoo.com>
2016-03-20 16:32:25 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,33 @@
+
+function startTime() {
+		var currentTime = new Date();
+		var hours = currentTime.getHours();
+		var minutes = currentTime.getMinutes();
+		var seconds = currentTime.getSeconds();
+
+		if (hours > 12) {
+			hours -= 12;
+		} else if (h === 0) {
+			h = 12;
+		}
+
+		function checkTime(i) {
+				if (i < 10) {
+					i = "0" + i;
+				}
+				return i;
+			}
+		minutes = checkTime(minutes);
+		seconds = checkTime(seconds);
+
+		document.getElementById('time').innerHTML = hours + ":" + minutes + ":" +seconds;
+
+		setTimeout(function () {
+			startTime()
+		}, 1000);	
+	
+}
+
+var clockStart = document.getElementById('time');
+clockStart.onload = startTime;
+	
